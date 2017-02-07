f390e23222019f70ce0bf5bbc0fc2999bcb83aec
kimbaol <qiaoxj07@163.com>
2017-02-07 08:33:36 +0800

    Init
    Init

---
+++ Assignment1/assignment1/cs231n/classifiers/linear_svm.py
@@ -0,0 +1,106 @@
+import numpy as np
+from random import shuffle
+
+def svm_loss_naive(W, X, y, reg):
+  """
+  Structured SVM loss function, naive implementation (with loops).
+
+  Inputs have dimension D, there are C classes, and we operate on minibatches
+  of N examples.
+
+  Inputs:
+  - W: A numpy array of shape (D, C) containing weights.
+  - X: A numpy array of shape (N, D) containing a minibatch of data.
+  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+    that X[i] has label c, where 0 <= c < C.
+  - reg: (float) regularization strength
+
+  Returns a tuple of:
+  - loss as single float
+  - gradient with respect to weights W; an array of same shape as W
+  """
+  dW = np.zeros(W.shape) # initialize the gradient as zero
+
+  # compute the loss and the gradient
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+  loss = 0.0
+  for i in xrange(num_train):
+    scores = X[i].dot(W)
+    correct_class_score = scores[y[i]]
+    for j in xrange(num_classes):
+      if j == y[i]:
+        continue
+      margin = scores[j] - correct_class_score + 1 # note delta = 1
+      if margin > 0:
+        loss += margin
+        dW[:,j] += X[i]
+    dW[:,y[i]] -= np.dot(dW, np.ones(num_classes))
+  # Right now the loss is a sum over all training examples, but we want it
+  # to be an average instead so we divide by num_train.
+  loss /= num_train
+  dW /= num_train
+
+  # Add regularization to the loss.
+  loss += 0.5 * reg * np.sum(W * W)
+  dW += reg * W
+  
+  #############################################################################
+  # TODO:                                                                     #
+  # Compute the gradient of the loss function and store it dW.                #
+  # Rather that first computing the loss and then computing the derivative,   #
+  # it may be simpler to compute the derivative at the same time that the     #
+  # loss is being computed. As a result you may need to modify some of the    #
+  # code above to compute the gradient.                                       #
+  #############################################################################
+
+
+  return loss, dW
+
+
+def svm_loss_vectorized(W, X, y, reg):
+  """
+  Structured SVM loss function, vectorized implementation.
+
+  Inputs and outputs are the same as svm_loss_naive.
+  """
+  loss = 0.0
+  dW = np.zeros(W.shape) # initialize the gradient as zero
+
+  #############################################################################
+  # TODO:                                                                     #
+  # Implement a vectorized version of the structured SVM loss, storing the    #
+  # result in loss.                                                           #
+  #############################################################################
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+  y_pred = X.dot(W)
+  correct_class_score = y_pred[range(num_train),y].reshape(num_train,-1)
+  Margin = y_pred - correct_class_score + np.ones([num_train,num_classes])
+  Margin[range(num_train),y] = 0
+  Margin_max =Margin[Margin>0]
+  loss = np.sum(Margin_max)/num_train + 0.5 * reg * np.sum(W * W)
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+
+
+  #############################################################################
+  # TODO:                                                                     #
+  # Implement a vectorized version of the gradient for the structured SVM     #
+  # loss, storing the result in dW.                                           #
+  #                                                                           #
+  # Hint: Instead of computing the gradient from scratch, it may be easier    #
+  # to reuse some of the intermediate values that you used to compute the     #
+  # loss.                                                                     #
+  #############################################################################
+  dMargin_mask = Margin > 0
+  dMargin_mask = dMargin_mask.astype(int,copy=False)
+  dMargin_mask[range(num_train),y] = -np.sum(dMargin_mask, axis=1)
+  dW = X.T.dot(dMargin_mask)/num_train + reg * W
+  
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+
+  return loss, dW
---
+++ Assignment1/assignment1/cs231n/classifiers/softmax.py
@@ -0,0 +1,93 @@
+import numpy as np
+from random import shuffle
+
+def softmax_loss_naive(W, X, y, reg):
+  """
+  Softmax loss function, naive implementation (with loops)
+
+  Inputs have dimension D, there are C classes, and we operate on minibatches
+  of N examples.
+
+  Inputs:
+  - W: A numpy array of shape (D, C) containing weights.
+  - X: A numpy array of shape (N, D) containing a minibatch of data.
+  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+    that X[i] has label c, where 0 <= c < C.
+  - reg: (float) regularization strength
+
+  Returns a tuple of:
+  - loss as single float
+  - gradient with respect to weights W; an array of same shape as W
+  """
+  # Initialize the loss and gradient to zero.
+  loss = 0.0
+  dW = np.zeros_like(W)
+  num_trains = X.shape[0]
+  num_features = W.shape[1]
+  #############################################################################
+  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
+  # Store the loss in loss and the gradient in dW. If you are not careful     #
+  # here, it is easy to run into numeric instability. Don't forget the        #
+  # regularization!                                                           #
+  #############################################################################
+  for i in xrange(num_trains):
+    scores = X[i].dot(W)
+    exp_scores = np.exp(scores)
+    sum_exp_scores = np.sum(exp_scores)
+    correct_exp_scores = exp_scores[y[i]]     
+    loss += -np.log(correct_exp_scores/sum_exp_scores)
+    for j in xrange(num_features):
+      score_per = exp_scores[j] / sum_exp_scores
+      if (j == y[i]):
+        dW[:,j] += (score_per - 1) * X[i].T
+      else :
+        dW[:,j] += score_per * X[i].T     
+    
+  loss /= num_trains
+  dW /= num_trains
+  loss += 0.5 * reg * np.sum(W*W)
+  dW += reg * W
+    
+  #############################################################################
+  #                          END OF YOUR CODE                                 #
+  #############################################################################
+
+  return loss, dW
+
+
+def softmax_loss_vectorized(W, X, y, reg):
+  """
+  Softmax loss function, vectorized version.
+
+  Inputs and outputs are the same as softmax_loss_naive.
+  """
+  # Initialize the loss and gradient to zero.
+  loss = 0.0
+  dW = np.zeros_like(W)
+  num_trains = X.shape[0]
+  num_features = W.shape[1]
+
+  #############################################################################
+  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
+  # Store the loss in loss and the gradient in dW. If you are not careful     #
+  # here, it is easy to run into numeric instability. Don't forget the        #
+  # regularization!                                                           #
+  #############################################################################
+  y_pred = X.dot(W)
+  exp_pred = np.exp(y_pred)
+  sum_exp_pred = np.sum(exp_pred, axis=1)
+  pred_per = exp_pred / sum_exp_pred[:,None]
+  loss = -np.sum(np.log(pred_per[range(num_trains),y]))
+  
+  
+  loss /= num_trains
+  dW /= num_trains
+  loss += 0.5 * reg * np.sum(W*W)
+  dW += reg * W
+  
+  #############################################################################
+  #                          END OF YOUR CODE                                 #
+  #############################################################################
+
+  return loss, dW
+
